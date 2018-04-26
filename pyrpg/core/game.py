e98956f5a278ccd3f270bf98a904013ccebcfbb6
Maximilian Klingmann <mkling@gothax.net>
2018-04-27 00:58:27 +0200

    Added walking animations and collision

--- pyrpg/core/game.py
+++ pyrpg/core/game.py
@@ -15,7 +15,9 @@ class LevelScene(taz.Scene):
         self.key_held = False
         self.screen = None
         self.map = None
-        self.render_surface = None
+        self.low_layers_render_surface = None
+        self.top_layer_render_surface = None
+        self.top_layer = None
         self.player = None
         self.scaling = None
         self.delta_accumulator = 0.0
@@ -23,19 +25,25 @@ class LevelScene(taz.Scene):
     def initialize(self):
         self.screen = self.game.render_context["screen"]
         self.map = self.load_map()
-        self.render_surface = pygame.Surface((self.map.width * self.map.tilewidth, self.map.height * self.map.tileheight))
-        self.scaling = [x/y for x, y in zip(self.screen.get_size(), self.render_surface.get_size())]
+        self.low_layers_render_surface = pygame.Surface((self.map.width * self.map.tilewidth, self.map.height * self.map.tileheight))
+        self.top_layer_render_surface = pygame.Surface((self.map.width * self.map.tilewidth, self.map.height * self.map.tileheight), pygame.SRCALPHA)
+        self.scaling = [x / y for x, y in zip(self.screen.get_size(), self.low_layers_render_surface.get_size())]
         self.player = self.load_player()
+
+        self.build_layer_surfaces()
 
     def render(self):
         if self.delta_accumulator >= self.game.render_context["RENDER_FREQUENCY"]:
             pygame.display.flip()
-            self.render_layers()
+
+            self.render_low_layers()
             self.render_player()
+            self.render_top_layer()
 
             self.delta_accumulator = 0.0
 
     def update(self):
+        pygame.display.set_caption("FPS: {}".format(int(self.game.update_context["clock"].get_fps())))
         delta = self.game.update_context["clock"].tick(self.game.update_context["UPDATE_FREQUENCY"])
         self.delta_accumulator += delta
 
@@ -68,14 +76,15 @@ class LevelScene(taz.Scene):
             if isinstance(layer, pytmx.TiledTileLayer):
                 yield layer
 
-    def render_layers(self):
-        for layer in self.get_layers():
-            self.render_layer(layer)
-        self.screen.blit(pygame.transform.scale(self.render_surface, self.screen.get_size()).convert(), (0, 0))
+    def render_low_layers(self):
+        self.screen.blit(pygame.transform.scale(self.low_layers_render_surface, self.screen.get_size()), (0, 0))
 
-    def render_layer(self, layer):
+    def render_top_layer(self):
+        self.screen.blit(pygame.transform.scale(self.top_layer_render_surface, self.screen.get_size()), (0, 0))
+
+    def render_layer(self, layer, surface):
         for x, y, tile in layer.tiles():
-            self.render_surface.blit(tile, (x * tile.get_width(), y * tile.get_height()))
+            surface.blit(tile, (x * tile.get_width(), y * tile.get_height()))
 
     def load_player(self):
         return player.Player(list(self.map.objectgroups), self.scaling, self.game)
@@ -88,6 +97,13 @@ class LevelScene(taz.Scene):
             self.game.render_context["key_held"] = True
             if not self.player.animation_running:
                 self.player.update(pygame.key.get_pressed(), delta)
+
+    def build_layer_surfaces(self):
+        for layer in self.get_layers():
+            if layer.name == "Tops":
+                self.render_layer(layer, self.top_layer_render_surface)
+            else:
+                self.render_layer(layer, self.low_layers_render_surface)
 
 
 
